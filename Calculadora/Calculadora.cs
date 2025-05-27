4bc0f7def2568616b6dec77a9eb6c2c52efbf787
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-05-27 14:54:44 -0300

    Base de la calculadora

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,40 @@
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+
+    private double dato;
+    void Sumar(double termino)
+    {
+
+    }
+
+    void Restar(double termino)
+    {
+
+    }
+
+    void Multiplicar(double termino)
+    {
+
+    }
+
+    void Dividir(double termino)
+    {
+
+    }
+
+    void Limpiar()
+    {
+
+    }
+
+    double Resultado
+    {
+        get
+        {
+            return dato;
+        }
+    }
+
+}
