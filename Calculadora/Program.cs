1534b3b08f5d151b0b373a93d782686578bed3a4
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-05-27 15:13:26 -0300

    Avance

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -2,11 +2,13 @@
 
 Calculadora c = new Calculadora();
 
-
 int opc = 0;
-
-while (true)
+double val = 0;
+ConsoleColor bg = Console.BackgroundColor;
+ConsoleColor fg = Console.ForegroundColor;
+while (opc != 6)
 {
+
     Console.WriteLine("=====================");
     Console.WriteLine(" 1 - Sumar");
     Console.WriteLine(" 2 - Restar");
@@ -15,17 +17,40 @@ while (true)
     Console.WriteLine(" 5 - Limpiar");
     Console.WriteLine(" 6 - Salir");
     Console.WriteLine("=====================");
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.BackgroundColor= ConsoleColor.Black;
+    Console.WriteLine("Valor: '" + c.Resultado + "'");
+    Console.ForegroundColor = fg;
+    Console.BackgroundColor = bg;
+    opc = LeerEntero();
 
+    if (opc <= 4)
+    {
+        val = LeerDouble("Ingresa el operando");
+    }
 
-}
+    switch (opc)
+    {
+        case 1:
+            c.Sumar(val);
+            break;
+        case 2:
+            c.Restar(val);
+            break;
+        case 3:
+            c.Multiplicar(val);
+            break;
+        case 4:
+            c.Dividir(val);
+            break;
+        case 5:
+            c.Limpiar();
+            break;
+        case 7:
+            PrintError("Opción incorrecta...");
+            break;
+    }
 
-
-
-static string LeerString(string texto = "Ingresa una cadena")
-{
-    Console.Write($"\n{texto}\n> ");
-    string ret = Console.ReadLine();
-    return ret ?? "";
 }
 
 static int LeerEntero(string texto = "Ingresá un número entero")
