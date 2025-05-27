fdf669656713505f5df9ed041bacafbcef3c8835
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-05-27 15:50:05 -0300

    Avance con la clase Empleado

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -4,22 +4,35 @@ namespace Sistema;
 
 public class Empleado
 {
-    public string nombre;
-    public string apellido;
-    public DateTime fechaNacimiento;
-    public char estadoCivil;
-    public DateTime fechaIngreso;
-    public double sueldoBasico;
-    public Cargos c;
 
+    ///////////////////////////////////////////////////// Fields
+    private string nombre;
+    private string apellido;
+    private DateTime fechaNacimiento;
+    private char estadoCivil;
+    private DateTime fechaIngreso;
+    private double sueldoBasico;
+    private Cargos cargo;
+
+
+    ///////////////////////////////////////////////////// Encapsulamiento
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Apellido { get =>apellido; set => apellido = value;}
+    public DateTime FechaNacimiento{ get => FechaNacimiento; set => fechaNacimiento = value; }
+    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+    public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+    public Cargos Cargo { get => cargo; set => cargo = value; }
+
+    ////////////////////////////////////////////////////////////////////
     public enum Cargos
     {
         Auxiliar, Administrativo, Ingeniero, Especialista, Investigador
     }
 
+    ////////////////////////////////////////////////////////////////////
     public int Antiguedad()
     {
-        return DateTime.Now.Year - this.fechaIngreso.Year;
+        return DateTime.Now.Year - this.FechaIngreso.Year;
     }
 
     public int Edad()
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,3 +1,10 @@
 ﻿using Sistema;
 
 Empleado e = new Empleado();
+
+e.FechaIngreso = new DateTime(2020, 1, 1);
+e.FechaNacimiento = new DateTime(1975, 02, 27);
+
+Console.WriteLine("Edad: " + e.Edad());
+Console.WriteLine("Años para jubilarse: " + e.AñosParaJubilarse());
+Console.WriteLine("Antiguedad: "+e.Antiguedad());
